2c8d20a54c7e573698bcea453ab37b013e73f1d0
inmok <ko3036@gmail.com>
2019-12-15 17:02:05 +0900

    수정 업로드

--- AWS_IoT_DHT11/AWS_IoT_DHT11/AWS_IoT_DHT11.ino
+++ AWS_IoT_DHT11/AWS_IoT_DHT11/AWS_IoT_DHT11.ino
@@ -19,23 +19,28 @@
 
   This example code is in the public domain.
 */
+#include <Arduino.h>
+#include <SPI.h>
+#include <Servo.h>
 
 #include <ArduinoBearSSL.h>
 #include <ArduinoECCX08.h>
 #include <ArduinoMqttClient.h>
-#include <WiFiNINA.h> // change to #include <WiFi101.h> for MKR1000
+#include <WiFiNINA.h>
 
 #include "arduino_secrets.h"
 
 #include "DHT.h"
-#define DHTPIN 2     // Digital pin connected to the DHT sensor
+#define DHTPIN 2        // 2번 핀을 온습도센서에 연결
 #define DHTTYPE DHT11   // DHT 11
 DHT dht(DHTPIN, DHTTYPE);
 
-#define LED_1_PIN 5
-
+Servo myservo;
+int motor1pin1=6;
+int motor1pin2=7;      //3,4번에 워터모터연결
+  
 #include <ArduinoJson.h>
-#include "Led.h"
+
 
 /////// Enter your sensitive data in arduino_secrets.h
 const char ssid[]        = SECRET_SSID;
@@ -48,12 +53,20 @@ BearSSLClient sslClient(wifiClient); // Used for SSL/TLS connection, integrates 
 MqttClient    mqttClient(sslClient);
 
 unsigned long lastMillis = 0;
+char* water_val = "OFF"; // 워터모터 ON,OFF 넣는 변수
+char* servo_val = "OFF"; // 서보모터 OPEN,HALFOPEN,CLOSE 넣는 변수
+int water_stand = 30;  // 워터모터 기준값 변수
 
-Led led1(LED_1_PIN);
 
 void setup() {
   Serial.begin(115200);
   while (!Serial);
+
+  myservo.attach(5);       //서보모터 5번
+
+  float sunlight = analogRead(A3);     // 조도센서 연결 
+  float soilmoisture = analogRead(A1); // 토양수분센서 연결
+
 
   dht.begin();
 
@@ -95,8 +108,8 @@ void loop() {
   // poll for new MQTT messages and send keep alives
   mqttClient.poll();
 
-  // publish a message roughly every 5 seconds.
-  if (millis() - lastMillis > 5000) {
+  // publish a message roughly every 10 seconds.
+  if (millis() - lastMillis > 10000) {
     lastMillis = millis();
     char payload[512];
     getDeviceStatus(payload);
@@ -140,19 +153,51 @@ void connectMQTT() {
   Serial.println("You're connected to the MQTT broker");
   Serial.println();
 
-  // subscribe to a topic
+  // delta 주제 구독
   mqttClient.subscribe("$aws/things/MyMKRWiFi1010/shadow/update/delta");
 }
 
-void getDeviceStatus(char* payload) {
+void getDeviceStatus(char* payload) {  // 이 함수도 10초마다 갱신
   // Read temperature as Celsius (the default)
-  float t = dht.readTemperature();
+  float temperature = dht.readTemperature();
+  float humidity = dht.readHumidity();
+  float soilmoisture = analogRead(A1);  // 습도센서 1번 연결
+  soilmoisture = map(soilmoisture,0,1023,100,0);
+  soilmoisture = constrain(soilmoisture,0,100); // 값을 0부터 100사이의 값으로
+  float sunlight = analogRead(A3);    // 조도센서 3번 연결
+  sunlight = map(sunlight,0,1023,100,0);
+  sunlight = constrain(sunlight,0,100);  // 값을 0부터 100사이의 값으로
 
-  // Read led status
-  const char* led = (led1.getState() == LED_ON)? "ON" : "OFF";
-
+  //워터 모터가 ON 이면서 수분값이 30 이상이면 작동을 멈춤
+  if (strcmp(water_val,"ON")==0 && soilmoisture >= water_stand ) {
+      pump(false);
+      delay(10000);
+      water_val="OFF";
+  }
+  //워터 모터가 ON 이면서 수분값이 30 미만이면 작동을 계속함
+  else if (strcmp(water_val,"ON")==0 && soilmoisture < water_stand ) {
+      pump(true);
+      delay(10000);
+      water_val="ON";
+  }
+  //워터 모터가 OFF 이면서 수분값이 30 이상이면 작동을 멈춤
+  else if (strcmp(water_val,"OFF")==0 && soilmoisture >= water_stand ) {
+      pump(false);
+      delay(10000);
+      water_val="OFF";
+  }
+  //워터 모터가 OFF 이면서 수분값이 30 미만이면 작동을 시작함
+  else if (strcmp(water_val,"OFF")==0 && soilmoisture < water_stand ) {
+      pump(true);
+      delay(10000);
+      water_val="ON";
+  }
+  
   // make payload for the device update topic ($aws/things/MyMKRWiFi1010/shadow/update)
-  sprintf(payload,"{\"state\":{\"reported\":{\"temperature\":\"%0.2f\",\"LED\":\"%s\"}}}",t,led);
+  sprintf(payload,"{\"state\":{\"reported\":{\"temperature\":\"%f\",\"humidity\":\"%f\",\"soilmoisture\":\"%f\",\"sunlight\":\"%f\",\"watermotor\":\"%s\",\"sunvisor\":\"%s\"}}}",temperature,humidity,soilmoisture,sunlight,water_val,"CLOSE");
+
+
+
 }
 
 void sendMessage(char* payload) {
@@ -204,20 +249,51 @@ void onMessageReceived(int messageSize) {
   deserializeJson(doc, buffer);
   JsonObject root = doc.as<JsonObject>();
   JsonObject state = root["state"];
-  const char* led = state["LED"];
-  Serial.println(led);
-  
+//  const char* water_val = state["watermoter"];
+//  const char* servo_val = state["sunvisor"];
+//  
   char payload[512];
   
-  if (strcmp(led,"ON")==0) {
-    led1.on();
-    sprintf(payload,"{\"state\":{\"reported\":{\"LED\":\"%s\"}}}","ON");
-    sendMessage(payload);
+  if (strcmp(water_val,"ON")==0) {
+     delay(10000);
+     sprintf(payload,"{\"state\":{\"reported\":{\"watermotor\":\"%s\"}}}",water_val);
+     sendMessage(payload);
     
-  } else if (strcmp(led,"OFF")==0) {
-    led1.off();
-    sprintf(payload,"{\"state\":{\"reported\":{\"LED\":\"%s\"}}}","OFF");
-    sendMessage(payload);
+  } else if (strcmp(water_val,"OFF")==0) {
+      delay(10000);
+      sprintf(payload,"{\"state\":{\"reported\":{\"watermotor\":\"%s\"}}}",water_val);
+      sendMessage(payload);
   }
+
+//   if (strcmp(servo_val,"OPEN")==0) {
+//      myservo.write(90);
+//      sprintf(payload,"{\"state\":{\"reported\":{\"sunvisor\":\"%s\"}}}",servo_val);
+//      sendMessage(payload);
+//    }
+//    else if (strcmp(servo_val,"HALFOPEN")==0) {
+//      myservo.write(45);
+//      sprintf(payload,"{\"state\":{\"reported\":{\"sunvisor\":\"%s\"}}}",servo_val);
+//      sendMessage(payload);
+//    }
+//    else if (strcmp(servo_val,"CLOSE")==0) {
+//      myservo.write(0);
+//      sprintf(payload,"{\"state\":{\"reported\":{\"sunvisor\":\"%s\"}}}",servo_val);
+//      sendMessage(payload);
+//    }
  
 }
+
+void pump(int flag)  // 펌프 돌아가는 함수
+  {
+    boolean inPin1;
+    boolean inPin2;
+  
+    if(flag){
+      inPin1 = HIGH;
+      inPin2 = LOW;
+    }
+    else{
+      inPin1 = LOW;
+      inPin2 = LOW;
+  }
+  }
